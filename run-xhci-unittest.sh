0600474aacdf5d061e7e5ad1e84194af6d376bee
Bjørn Forsman <bf@hiddn.no>
2016-12-01 16:54:52 +0100

    Add run-xhci-unittest.sh

---
+++ run-xhci-unittest.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# TODO: Use a real unittest framework and find a proper location for the source
+# code for the tests.
+cd grub-core/bus/usb/ && \
+  make -f build_xhci_test.mk && \
+  ./xhci_unittest && \
+  echo $?
