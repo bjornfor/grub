7606ee7c81d9204a2b9e45610fd537728b55918b
Bjørn Forsman <bf@hiddn.no>
2016-12-02 15:51:49 +0100

    xhci: unittest: print build warnings

--- grub-core/bus/usb/build_xhci_test.mk
+++ grub-core/bus/usb/build_xhci_test.mk
@@ -1,10 +1,11 @@
 # Expected to be invoked from ./grub-core/bus/usb.
 
 CC ?= gcc
+CFLAGS ?= -Wall
 SRCS = xhci_unittest.c xhci.c xhci_io_mock.c
 PROG = xhci_unittest
 
 all: $(PROG)
 
 $(PROG): $(SRCS)
-	$(CC) $^ -o $(PROG)
+	$(CC) $(CFLAGS) $^ -o $(PROG)
